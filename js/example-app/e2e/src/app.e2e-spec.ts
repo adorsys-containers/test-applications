ad00f73d189d28bc0bcaf8de2e41a5595792f6ac
Jan-Otto Kröpke <joe@adorsys.de>
2020-12-11 21:19:55 +0100

    Update *

--- js/example-app/e2e/src/app.e2e-spec.ts
+++ js/example-app/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to test-app!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('example-app app is running!');
   });
 
   afterEach(async () => {
